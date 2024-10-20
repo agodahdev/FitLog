2c4969427ee0a99564a24634b19f886fc76851f7
Ahmed Godah <godahahmed@gmail.com>
2024-10-20 16:07:56 +0000

    fixed issues on workout file

--- src/workout.py
+++ src/workout.py
@@ -1,12 +1,12 @@
 #workout class to store workout information
 class Workout:
-    def__init__(self, exercise, duration, intensity, calories_burned, date):
-    self.exercise = exercise
-    self.duration = duration
-    self.intensity = intensity
-    self.calories_burned = calories_burned
-    self.date = date
+    def __init__(self, exercise, duration, intensity, calories_burned, date):
+        self.exercise = exercise
+        self.duration = duration
+        self.intensity = intensity
+        self.calories_burned = calories_burned
+        self.date = date
 
-    # string for printing the workout 
-    def__str__(self):
-       return f"{self.date}: {self.excercise} for {self.duration} mins ({self.intensity} intensity) - {self.calories_burned} kcal"
+    # String representation of the workout for easy printing
+    def __str__(self):
+        return f"{self.date}: {self.exercise} for {self.duration} mins ({self.intensity} intensity) - {self.calories_burned} kcal"
